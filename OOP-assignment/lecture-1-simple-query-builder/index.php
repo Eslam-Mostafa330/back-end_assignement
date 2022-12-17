1f3618f75f34c9a68a8419864b2c8c1763730383
Eslam-Mostafa330 <104284384+Eslam-Mostafa330@users.noreply.github.com>
2022-12-17 03:18:53 +0200

    Update index.php

--- OOP-assignment/lecture-1-simple-query-builder/index.php
+++ OOP-assignment/lecture-1-simple-query-builder/index.php
@@ -25,7 +25,7 @@ $db = new DB;
 // $data = $db->table("users")->select("email")->where('id', '=', 5)->or('id', '=', 6)->all();
 
 ## Inner Join Query ##
-// $data = $db->table("users")->select("users.id, products.name")->join("products")->on('users.id', '=', 'products.id')->all();
+$data = $db->table("users")->select("users.id, products.name")->join("products")->on('users.id', '=', 'products.id')->all();
 
 echo "<pre>";
 print_r($data);
